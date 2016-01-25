9bfe71f703a26e4dd5f584d9cf0b95672b25427e
lasttry <rsantos@parallels.com>
2016-01-25 11:04:03 +0000

    first

---
+++ APS.CSharp/MainForm.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace APS.CSharp
+{
+    partial class frmAPSCSharp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtWorkingFolder = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.fbdBinFolder = new System.Windows.Forms.FolderBrowserDialog();
+            this.lblInfos = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lvLibraries = new System.Windows.Forms.ListView();
+            this.lvClasses = new System.Windows.Forms.ListView();
+            this.chAssembly = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chClasses = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lsbDetails = new System.Windows.Forms.ListBox();
+            this.txtSchema = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtWorkingFolder
+            // 
+            this.txtWorkingFolder.Location = new System.Drawing.Point(12, 49);
+            this.txtWorkingFolder.Name = "txtWorkingFolder";
+            this.txtWorkingFolder.Size = new System.Drawing.Size(406, 20);
+            this.txtWorkingFolder.TabIndex = 0;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // lblInfos
+            // 
+            this.lblInfos.AutoSize = true;
+            this.lblInfos.Location = new System.Drawing.Point(12, 9);
+            this.lblInfos.Name = "lblInfos";
+            this.lblInfos.Size = new System.Drawing.Size(406, 13);
+            this.lblInfos.TabIndex = 1;
+            this.lblInfos.Text = "In order to create the schemas of your APS you first need to select the working f" +
+    "older";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Working Folder";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(426, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(30, 19);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lvLibraries
+            // 
+            this.lvLibraries.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.chAssembly});
+            this.lvLibraries.Location = new System.Drawing.Point(12, 75);
+            this.lvLibraries.Name = "lvLibraries";
+            this.lvLibraries.Size = new System.Drawing.Size(238, 218);
+            this.lvLibraries.TabIndex = 4;
+            this.lvLibraries.UseCompatibleStateImageBehavior = false;
+            this.lvLibraries.View = System.Windows.Forms.View.Details;
+            this.lvLibraries.SelectedIndexChanged += new System.EventHandler(this.lvLibraries_SelectedIndexChanged);
+            // 
+            // lvClasses
+            // 
+            this.lvClasses.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.chClasses});
+            this.lvClasses.Location = new System.Drawing.Point(256, 75);
+            this.lvClasses.Name = "lvClasses";
+            this.lvClasses.Size = new System.Drawing.Size(238, 218);
+            this.lvClasses.TabIndex = 5;
+            this.lvClasses.UseCompatibleStateImageBehavior = false;
+            this.lvClasses.View = System.Windows.Forms.View.Details;
+            this.lvClasses.SelectedIndexChanged += new System.EventHandler(this.lvClasses_SelectedIndexChanged);
+            // 
+            // chAssembly
+            // 
+            this.chAssembly.Text = "Assemblies";
+            this.chAssembly.Width = 230;
+            // 
+            // chClasses
+            // 
+            this.chClasses.Text = "Classes";
+            this.chClasses.Width = 230;
+            // 
+            // lsbDetails
+            // 
+            this.lsbDetails.FormattingEnabled = true;
+            this.lsbDetails.Location = new System.Drawing.Point(500, 75);
+            this.lsbDetails.Name = "lsbDetails";
+            this.lsbDetails.Size = new System.Drawing.Size(340, 381);
+            this.lsbDetails.TabIndex = 6;
+            // 
+            // txtSchema
+            // 
+            this.txtSchema.Location = new System.Drawing.Point(846, 12);
+            this.txtSchema.Multiline = true;
+            this.txtSchema.Name = "txtSchema";
+            this.txtSchema.Size = new System.Drawing.Size(502, 532);
+            this.txtSchema.TabIndex = 7;
+            // 
+            // frmAPSCSharp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1360, 556);
+            this.Controls.Add(this.txtSchema);
+            this.Controls.Add(this.lsbDetails);
+            this.Controls.Add(this.lvClasses);
+            this.Controls.Add(this.lvLibraries);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblInfos);
+            this.Controls.Add(this.txtWorkingFolder);
+            this.Name = "frmAPSCSharp";
+            this.Text = "APS.CSharp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtWorkingFolder;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog fbdBinFolder;
+        private System.Windows.Forms.Label lblInfos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView lvLibraries;
+        private System.Windows.Forms.ListView lvClasses;
+        private System.Windows.Forms.ColumnHeader chAssembly;
+        private System.Windows.Forms.ColumnHeader chClasses;
+        private System.Windows.Forms.ListBox lsbDetails;
+        private System.Windows.Forms.TextBox txtSchema;
+    }
+}
+
---
+++ APS.CSharp/MainForm.cs
@@ -0,0 +1,124 @@
+﻿using APS.CSharp.SDK.Attributes;
+using APS.CSharp.VSPlugin;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace APS.CSharp
+{
+    public partial class frmAPSCSharp : Form
+    {
+
+        private Assembly currentAssembly;
+
+        public frmAPSCSharp()
+        {
+            InitializeComponent();
+            this.txtWorkingFolder.Text = string.IsNullOrEmpty(Properties.Settings.Default.CurrentFolder) ? System.IO.Directory.GetCurrentDirectory() : Properties.Settings.Default.CurrentFolder;
+            this.fbdBinFolder.SelectedPath = this.txtWorkingFolder.Text;
+            FillLibraries();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = fbdBinFolder.ShowDialog(this);
+            if(dr == DialogResult.OK)
+            {
+                this.txtWorkingFolder.Text = fbdBinFolder.SelectedPath;
+                Properties.Settings.Default.CurrentFolder = fbdBinFolder.SelectedPath;
+                Properties.Settings.Default.Save();
+                FillLibraries();
+            }
+        }
+
+        private void FillLibraries()
+        {
+            this.lvLibraries.Items.Clear();
+            currentAssembly = null;
+            
+            foreach (string file in Directory.GetFiles(this.txtWorkingFolder.Text, "*.dll"))
+            {
+                ListViewItem lvi = new ListViewItem();
+                lvi.Text = Path.GetFileName(file);
+                lvi.Name = file;
+                this.lvLibraries.Items.Add(lvi);
+            }
+        }
+
+        private void lvLibraries_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            currentAssembly = null;
+            lvClasses.Items.Clear();
+
+            if (lvLibraries.SelectedItems.Count == 0)
+                return;
+
+            currentAssembly = Assembly.LoadFrom(Path.Combine(this.txtWorkingFolder.Text, lvLibraries.SelectedItems[0].Text));
+            foreach (Type t in currentAssembly.GetTypes())
+                if(t.GetCustomAttributes<ResourceBaseAttribute>().Count() > 0)
+                this.lvClasses.Items.Add(new ListViewItem(t.FullName));
+        }
+
+        private void lvClasses_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.lsbDetails.Items.Clear();
+
+            if (currentAssembly == null)
+                return;
+
+            if (lvClasses.SelectedItems.Count == 0)
+                return;
+
+            Type type = currentAssembly.GetType(lvClasses.SelectedItems[0].Text);
+
+            Generator g = new Generator();
+            txtSchema.Text = g.Generate(type);
+
+
+
+            ResourceBaseAttribute resourceAttr = type.GetCustomAttribute<SDK.Attributes.ResourceBaseAttribute>();
+
+            resourceAttr = type.GetCustomAttribute(typeof(ResourceBaseAttribute), false) as ResourceBaseAttribute;
+            if (resourceAttr == null)
+                return;
+
+
+
+            this.lsbDetails.Items.Add("apsVersion: " + resourceAttr.ApsVersion);
+            this.lsbDetails.Items.Add("id: " + resourceAttr.Id);
+            if(resourceAttr.Implements != null)
+                this.lsbDetails.Items.Add("implements: " + String.Join(",", resourceAttr.Implements));
+
+            List<PropertyInfo> p = new List<PropertyInfo>();
+            List<PropertyInfo> l = new List<PropertyInfo>();
+            List<PropertyInfo> s = new List<PropertyInfo>();
+
+            foreach(PropertyInfo property in type.GetProperties(BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.Instance))
+            {
+                if (property.GetCustomAttribute<SDK.Attributes.LinkAttribute>() != null)
+                    l.Add(property);
+                else if (property.GetCustomAttribute<SDK.Attributes.StructureAttribute>() != null)
+                    s.Add(property);
+                else
+                    p.Add(property);
+            }
+            FillListItems(p, ">P>");
+            FillListItems(l, ">L>");
+            FillListItems(s, ">S>");
+        }
+
+        private void FillListItems(List<PropertyInfo> properties, string prefix)
+        {
+            foreach (PropertyInfo property in properties)
+                this.lsbDetails.Items.Add(prefix + property.Name);
+        }
+    }
+}
